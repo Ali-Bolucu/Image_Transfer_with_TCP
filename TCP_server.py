4df02d4b09eddf3eadd85bfb6305242530a201b5
Ali Bölücü <48453081+Ali-Bolucu@users.noreply.github.com>
2023-05-07 17:36:04 +0300

    Update TCP_server.py

--- TCP_server.py
+++ TCP_server.py
@@ -1,7 +1,6 @@
 import socket
 import os
 from _thread import *
-import base64
 
 
 absolute_path = os.path.dirname(os.path.abspath(__file__))
@@ -19,7 +18,50 @@ print ("socket binded to %s" %(PORT))
 server_socket.listen() # how many ports to listen
 print(f"Server is listening on {HOST}:{PORT}...")
 
+def recv(full_path):
+     
 
+    file = open(full_path, "wb")
+
+    imageSize = int(client_socket.recv(15).decode('latin-1'))
+
+    
+    totalPacketSize = 9 + 10240 + 9
+    TCP_PacketNumber = 1
+    packetSize = 10240
+    localChecksum = 0
+    TCP_Checksum = 0
+
+    TCP_BytesReceived = 0
+    TCP_TotalBytesReceived = 0
+    TCP_TotalPacketNumber = imageSize // packetSize + (1 if imageSize % packetSize > 0 else 0)
+
+    print(imageSize)
+    print(TCP_TotalPacketNumber)
+    print("\n")
+    
+    while not (TCP_TotalPacketNumber == int(TCP_PacketNumber)) :
+        
+        TCP_PacketNumber = client_socket.recv(9).decode('latin-1')
+        TCP_PacketNumber = TCP_PacketNumber.lstrip("0")
+        
+        imageData = client_socket.recv(10240)
+        
+        TCP_Checksum = client_socket.recv(9).decode('latin-1')
+        TCP_Checksum = TCP_Checksum.lstrip("0")
+        
+        print(TCP_PacketNumber)
+        print(TCP_Checksum)
+        print("\n")
+        file.write(imageData)
+        
+        print()
+        
+    print("end")
+    file.close()
+
+        
+    
 
 
 def multi_threaded_client(client_socket):
@@ -54,12 +96,12 @@ def multi_threaded_client(client_socket):
             print( "ImageSize :" + str(imageSize))
             print(type(imageSize))
             print("\n")
-            client_socket.sendall((str(imageSize)).encode())
+            client_socket.send((str(imageSize)).encode())
             imageData = file.read()
    
-            totalPacketSize = 9 + 1024 + 9
+            totalPacketSize = 9 + 10240 + 9
             TCP_PacketNumber = 1
-            packetSize = 1024
+            packetSize = 10240
             localChecksum = 0
             TCP_Checksum = 0
 
@@ -107,33 +149,11 @@ def multi_threaded_client(client_socket):
         if data[:3] == 'Rec':
             data = data.replace("\0", "")
             print(f"[Pic] {data[3:]}")
-        
+
             relative_path  = f'pictures\\{data[3:]}'
-            full_path = os.path.join(absolute_path, relative_path)           
-
-            file = open(full_path, "wb")
-
-            #size = client_socket.recv(1024).decode()
-            #print(size)
+            full_path = os.path.join(absolute_path, relative_path)      
+            recv(full_path)
             
-            END_OF_TRANSMISSION = b'END'
-
-            image = client_socket.recv(1024)
-            
-            while not image.endswith(END_OF_TRANSMISSION):
-                file.write(image)
-                image = client_socket.recv(1024)
-
-                    
-            
-            #file.write(image)
-            file.close()
-
-        
-        if (server_message == "close") or (server_message == "exit"):
-            print("closing")
-            server_socket.close()
-        
         
 #---------------------------------------------------
         
