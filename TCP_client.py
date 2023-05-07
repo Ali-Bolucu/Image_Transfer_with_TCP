cc62ed45c0911792cfba040512994c8eef61f9d1
Ali Bölücü <48453081+Ali-Bolucu@users.noreply.github.com>
2023-05-07 17:35:21 +0300

    Update TCP_client.py

--- TCP_client.py
+++ TCP_client.py
@@ -2,7 +2,7 @@ import time
 import socket
 import os
 
-HOST = "31.223.50.236"  # The server's hostname or IP address
+HOST = "127.0.0.1"  # The server's hostname or IP address
 PORT = 8079  # The port used by the server
 
 absolute_path = os.path.dirname(os.path.abspath(__file__))
@@ -30,29 +30,55 @@ with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as client_socket:
 
 			file_name = file_list[file_num-1]
 			file_path = os.path.join(folder_path, file_name)
+   
+			file = open(file_path, 'rb')
+			imageSize = os.path.getsize(file_path)
 
 			message = "Rec" + file_name
 			print(message)
 			client_socket.send(message.encode('latin-1'))
+   
+			str_imageSize = '{:0>15}'.format(str(imageSize))
+			client_socket.send(str_imageSize.encode())
+			print(imageSize)
+   
+			totalPacketSize = 9 + 10240 + 9
+			TCP_PacketNumber = 1
+			packetSize = 10240
+			localChecksum = 0
+			TCP_Checksum = 0
 
-			file = open(file_path, 'rb')
-			file_size = os.path.getsize(file_path)
-			
-			print(file_size)
-			print(type(file_size))
-			
-			#client_socket.sendall((str(file_size)).encode())
-			
-			picture_data = file.read(1024)
+			TCP_BytesReceived = 0
+			TCP_TotalBytesReceived = 0
+			TCP_TotalPacketNumber = imageSize // packetSize + (1 if imageSize % packetSize > 0 else 0)
 
-			#client_socket.sendall(picture_data)
-			END_OF_TRANSMISSION = b'END'
+			imageData = file.read()
 
-			while picture_data:
-				client_socket.send(picture_data)
-				picture_data = file.read(1024)
-    
-			client_socket.send(END_OF_TRANSMISSION)
+			for i in range(0, len(imageData) , packetSize):
+							
+				str_TCP_PacketNumber = '{:0>9}'.format(str(TCP_PacketNumber))
+				client_socket.send(str_TCP_PacketNumber.encode())
+				print( str(TCP_PacketNumber))
+				TCP_PacketNumber += 1
+				
+				packet = imageData[i:i+packetSize]
+				# for the last packet
+				if len(packet) < packetSize:
+					packet += b'\x00' * (packetSize - len(packet))
+				client_socket.send(packet)
+
+						
+				TCP_Checksum = 0
+				for a in range(0, packetSize):
+					TCP_Checksum += packet[a]
+				str_TCP_Checksum = '{:0>9}'.format(str(TCP_Checksum))
+				client_socket.send(str_TCP_Checksum.encode())
+
+				print( str(TCP_Checksum))
+				
+				#ACK_NCK = client_socket.recv(4).decode('latin-1')
+				#print(ACK_NCK + "\n")
+				
 			file.close()
 		
 		if (file_num == "close") or (file_num == "exit"):
